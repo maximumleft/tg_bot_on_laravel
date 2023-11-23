d2a059964337fcd1371d4e2a24a332fd14cc0591
Maksim <mdlevsha@gmail.com>
2023-11-23 15:56:02 +0300

    chatid not work

--- app/Http/Telegraph/Answer.php
+++ app/Http/Telegraph/Answer.php
@@ -2,19 +2,59 @@
 
 namespace App\Http\Telegraph;
 
+use DefStudio\Telegraph\Facades\Telegraph;
 use DefStudio\Telegraph\Handlers\WebhookHandler;
+use DefStudio\Telegraph\Keyboard\Button;
+use DefStudio\Telegraph\Keyboard\Keyboard;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Stringable;
 
 class Answer extends WebhookHandler
 {
 
-    public function start()
+    public function start(): void
     {
-        $this->chat->html(__('Welcome!'))->send();
+        Telegraph::message('Кто ты, воин?')
+            ->Keyboard(
+                Keyboard::make()->buttons([
+                    Button::make('Руслан')->action('ruslan'),
+                    Button::make('Федос')->action('fedos'),
+                    Button::make('Сергей')->action('sergey'),
+                    Button::make('Максончик')->action('maks'),
+                ])
+            )->send();
     }
 
-    public function hello()
+    public function ruslan(): void
     {
-        $this->chat->html(__('Текст'))->send();
+        $this->chat->audio('C:\Projects_laravel\tg_bot_telegraph\content\songs\Мэйби Бэйби - DURAGA.mp3')->send();
+    }
+    public function fedos(): void
+    {
+        $this->chat->audio('C:\Projects_laravel\tg_bot_telegraph\content\songs\trinadcat_karat_-_davajj_rasskazhem_(musmore.com).mp3')->send();
+    }
+    public function sergey(): void
+    {
+        $this->chat->audio('C:\Projects_laravel\tg_bot_telegraph\content\songs\sqwoz-bab-300-mp3.mp3')->send();
+    }
+    public function maks(): void
+    {
+        $this->chat->audio('C:\Projects_laravel\tg_bot_telegraph\content\songs\sqwoz-bab-300-mp3.mp3')->send();
+    }
+
+    public function hello(): void
+    {
+        $this->chat->photo('C:\Projects_laravel\tg_bot_telegraph\images\hello.jpg')->send();
+    }
+
+    public function help(): void
+    {
+        $this->reply('Говорил же не поможет');
+    }
+
+    protected function handleChatMessage(Stringable $text): void
+    {
+        Log::info(json_encode($this->message->toArray(), JSON_UNESCAPED_UNICODE));
     }
 }
 
--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use DefStudio\Telegraph\Models\TelegraphBot;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,13 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('tester', function () {
+    /**  @var TelegraphBot $bot */
+
+    $bot = TelegraphBot::find(1);
+    dd($bot->registerCommands([
+        'hello' => 'говорит здарова',
+        'help' => 'не поможет',
+    ])->send());
+});
